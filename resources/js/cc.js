e82251d9659e00802ab320436fef53180267f0b8
GeoffreyH <69577362+GeoffreyHeu@users.noreply.github.com>
2020-11-13 19:31:52 +0100

    push

---
+++ resources/js/cc.js
@@ -0,0 +1,21 @@
+var canele = 0;
+var four = 1;
+
+function NewCanele() {
+    canele += four;
+    let paragraphe = "Vous avez " + canele.toString() + " canelés";
+    document.getElementById("canelé").innerHTML = paragraphe;
+}
+
+function NewFour() {
+    if(canele >= 25) {
+        canele -= 25;
+        four += 1;
+        let paragraphe = "Vous avez " + canele.toString() + " canelé";
+        document.getElementById("canelé").innerHTML = paragraphe;
+        alert("Vous avez un nouveau Four !");
+    }
+    else {
+        alert("Vous n'avez pas assez de canelé !");
+    }
+}
